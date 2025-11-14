91016fafb46065d88efc5e11821f99e2676a723e
YoungP0p <youngp219@wku.ac.kr>
2025-11-14 15:09:57 +0900

    Add files via upload

---
+++ hw11/main.js
@@ -0,0 +1,27 @@
+const menuItems = document.querySelectorAll(".menu-item");
+let currentIndex = 0;
+
+setActive(currentIndex);
+
+function setActive(idx) {
+    menuItems.forEach((item, i) => {
+        if (i === idx) item.classList.add("select");
+        else item.classList.remove("select");
+    });
+}
+
+window.addEventListener("keydown", (e) => {
+    if (e.key === "ArrowUp") {
+        currentIndex--;
+        if (currentIndex < 0) currentIndex = menuItems.length - 1;
+        setActive(currentIndex);
+    }
+    else if (e.key === "ArrowDown") {
+        currentIndex++;
+        if (currentIndex >= menuItems.length) currentIndex = 0;
+        setActive(currentIndex);
+    }
+    else if (e.key === "Enter") {
+        alert(`"${menuItems[currentIndex].textContent}" 선택됨`);
+    }
+});
